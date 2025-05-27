396aa949fe64ae611136c19ca20d93ff56200d85
Nicolas Ruflin <spam@ruflin.com>
2025-05-27 10:58:53 +0200

    Fixes nil panic because of rand not being thread save
    As soon as metricsgenreceiver is run with concurrency > 0 for a scenarion, eventually a panic in the rand generator is caused. The reason is that rand is not thread safe, also see: https://github.com/golang/go/issues/66086
    
    The fix is instead of having a single rand for the receiver, a rand object is instantiated per go routine. As the same seed is used, this should still hav exactly the same outcome.
    
    The following panic was caused.
    
    ```
    2025-05-27T10:55:30.787+0200	info	metricsgenreceiver@v0.123.0/receiver.go:119	generating metrics progress	{"progress_percent": 0, "eta": "24m7s", "datapoints": 1008000, "data_points_per_second": 100093.19482744727}
    panic: runtime error: index out of range [-1]
    
    goroutine 1077 [running]:
    math/rand.(*rngSource).Uint64(...)
    	math/rand/rng.go:249
    math/rand.(*rngSource).Int63(0x14016f91a88?)
    	math/rand/rng.go:234 +0x8c
    math/rand.(*Rand).Int63(...)
    	math/rand/rand.go:96
    math/rand.(*Rand).Uint32(...)
    	math/rand/rand.go:99
    math/rand.(*Rand).NormFloat64(0x140003408a0)
    	math/rand/normal.go:39 +0x38
    github.com/elastic/metricsgenreceiver/metricsgenreceiver/internal/distribution.advanceFloat(0x140003408a0?, {0x1400381fc68, 0x140376d35b8}, 0x403da5a9aea86617, {0x40?, 0x1400044c920?, 0x1f8014000030828?})
    	github.com/elastic/metricsgenreceiver/metricsgenreceiver@v0.123.0/internal/distribution/distribution.go:88 +0x30
    github.com/elastic/metricsgenreceiver/metricsgenreceiver/internal/distribution.AdvanceDataPoint({0x106bb0d40?, 0x1403735f4a0?}, 0x14016f91c01?, {0x1400381fc68?, 0x140376d35b8?}, {0x1061e2134?, 0x10?, 0x106ae9380?})
    	github.com/elastic/metricsgenreceiver/metricsgenreceiver@v0.123.0/internal/distribution/distribution.go:37 +0x31c
    github.com/elastic/metricsgenreceiver/metricsgenreceiver.(*MetricsGenReceiver).produceMetricsForInstance.func1({0x106ae9380?, 0x14016f91d98?}, {0x14016f91cb8?, 0x105258a74?}, {0x1400381fc68?, 0x140376d35b8?}, {0x106bb0d40, 0x1403735f4a0})
    	github.com/elastic/metricsgenreceiver/metricsgenreceiver@v0.123.0/receiver.go:221 +0x68
    github.com/elastic/metricsgenreceiver/metricsgenreceiver/internal/dp.ForEachDataPoint(0x14000362cd8?, 0x14016f91e48)
    	github.com/elastic/metricsgenreceiver/metricsgenreceiver@v0.123.0/internal/dp/datapoint.go:23 +0x628
    github.com/elastic/metricsgenreceiver/metricsgenreceiver.(*MetricsGenReceiver).produceMetricsForInstance(0x140002f60e0, {0x106b9bc50, 0x1400072f450}, {0x14002895748?, 0x140028957a8?, 0x107fd68e0?}, {{{0x1400013aa08, 0x13}, 0x64, 0x5, ...}, ...}, ...)
    	github.com/elastic/metricsgenreceiver/metricsgenreceiver@v0.123.0/receiver.go:220 +0x12c
    github.com/elastic/metricsgenreceiver/metricsgenreceiver.(*MetricsGenReceiver).produceMetrics.func2()
    	github.com/elastic/metricsgenreceiver/metricsgenreceiver@v0.123.0/receiver.go:202 +0x12c
    created by github.com/elastic/metricsgenreceiver/metricsgenreceiver.(*MetricsGenReceiver).produceMetrics in goroutine 50
    	github.com/elastic/metricsgenreceiver/metricsgenreceiver@v0.123.0/receiver.go:198 +0x1b4
    ```

--- metricsgenreceiver/receiver.go
+++ metricsgenreceiver/receiver.go
@@ -28,7 +28,6 @@ type MetricsGenReceiver struct {
 	settings  receiver.Settings
 
 	nextMetrics consumer.Metrics
-	rand        *rand.Rand
 	cancel      context.CancelFunc
 	scenarios   []Scenario
 }
@@ -48,7 +47,7 @@ func newMetricsGenReceiver(cfg *Config, set receiver.Settings) (*MetricsGenRecei
 	if err != nil {
 		return nil, err
 	}
-	r := rand.New(rand.NewSource(cfg.Seed))
+
 	nowish := time.Now().Truncate(time.Second)
 	if cfg.StartTime.IsZero() {
 		cfg.StartTime = nowish.Add(-cfg.StartNowMinus)
@@ -56,6 +55,8 @@ func newMetricsGenReceiver(cfg *Config, set receiver.Settings) (*MetricsGenRecei
 	if cfg.EndTime.IsZero() {
 		cfg.EndTime = nowish.Add(-cfg.EndNowMinus)
 	}
+
+	ra := rand.New(rand.NewSource(cfg.Seed))
 
 	scenarios := make([]Scenario, 0, len(cfg.Scenarios))
 	for _, scn := range cfg.Scenarios {
@@ -79,7 +80,7 @@ func newMetricsGenReceiver(cfg *Config, set receiver.Settings) (*MetricsGenRecei
 				}
 			}
 		})
-		resources, err := metricstmpl.GetResources(scn.Path, cfg.StartTime, scn.Scale, scn.TemplateVars, r)
+		resources, err := metricstmpl.GetResources(scn.Path, cfg.StartTime, scn.Scale, scn.TemplateVars, ra)
 		if err != nil {
 			return nil, err
 		}
@@ -94,7 +95,6 @@ func newMetricsGenReceiver(cfg *Config, set receiver.Settings) (*MetricsGenRecei
 		cfg:       cfg,
 		settings:  set,
 		obsreport: obsreport,
-		rand:      r,
 		scenarios: scenarios,
 	}, nil
 }
@@ -163,10 +163,12 @@ func (r *MetricsGenReceiver) applyChurn(interval int, simulatedTime time.Time) {
 			continue
 		}
 
+		ra := r.getRand()
+
 		startTime := simulatedTime.Format(time.RFC3339)
 		for i := 0; i < scn.config.Churn; i++ {
 			id := scn.config.Scale + interval*scn.config.Churn + i
-			resource, err := metricstmpl.RenderResource(scn.config.Path, id, startTime, scn.config.TemplateVars, r.rand)
+			resource, err := metricstmpl.RenderResource(scn.config.Path, id, startTime, scn.config.TemplateVars, ra)
 			if err != nil {
 				r.settings.Logger.Error("failed to apply churn", zap.Error(err))
 			} else {
@@ -180,11 +182,13 @@ func (r *MetricsGenReceiver) produceMetrics(ctx context.Context, currentTime tim
 	dataPoints := new(uint64)
 	wg := sync.WaitGroup{}
 	for _, scn := range r.scenarios {
+		ra := r.getRand()
+
 		// we don't keep track of the data points for each instance individually to reduce memory pressure
 		// we still advance the metrics template have a new baseline that's used when simulating the metrics for each individual instance
 		// this makes sure counters are increasing over time
 		dp.ForEachDataPoint(scn.metricsTemplate, func(res pcommon.Resource, is pcommon.InstrumentationScope, m pmetric.Metric, dp dp.DataPoint) {
-			distribution.AdvanceDataPoint(dp, r.rand, m, r.cfg.Distribution)
+			distribution.AdvanceDataPoint(dp, ra, m, r.cfg.Distribution)
 		})
 		if scn.config.Concurrency == 0 {
 			for i := range scn.config.Scale {
@@ -196,6 +200,7 @@ func (r *MetricsGenReceiver) produceMetrics(ctx context.Context, currentTime tim
 		for i := 0; i < scn.config.Concurrency; i++ {
 			wg.Add(1)
 			go func() {
+
 				defer wg.Done()
 				for j := 0; j < scn.config.Scale/scn.config.Concurrency; j++ {
 					resource := scn.resources[j+i*scn.config.Scale/scn.config.Concurrency]
@@ -217,8 +222,11 @@ func (r *MetricsGenReceiver) produceMetricsForInstance(ctx context.Context, curr
 	for j := 0; j < resourceMetrics.Len(); j++ {
 		overrideExistingAttributes(instanceResource, resourceMetrics.At(j).Resource())
 	}
+
+	ra := r.getRand()
+
 	dp.ForEachDataPoint(&metrics, func(res pcommon.Resource, is pcommon.InstrumentationScope, m pmetric.Metric, dp dp.DataPoint) {
-		distribution.AdvanceDataPoint(dp, r.rand, m, r.cfg.Distribution)
+		distribution.AdvanceDataPoint(dp, ra, m, r.cfg.Distribution)
 		dp.SetTimestamp(pcommon.NewTimestampFromTime(currentTime))
 	})
 	dataPoints := metrics.DataPointCount()
@@ -243,4 +251,10 @@ func (r *MetricsGenReceiver) Shutdown(_ context.Context) error {
 		r.cancel()
 	}
 	return nil
+}
+
+// getRand returns a new random number generator seeded with the configured seed.
+// This is NOT thread-safe, so it should only be used in a single goroutine.
+func (r *MetricsGenReceiver) getRand() *rand.Rand {
+	return rand.New(rand.NewSource(r.cfg.Seed))
 }
